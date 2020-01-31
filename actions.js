6c49bef399398c6bc92ae83dc12b83dac36de9a1
Monty Schmidt <monty@wuntusk.com>
2020-01-31 07:56:34 -0500

    initial

---
+++ actions.js
@@ -0,0 +1,10 @@
+async function hello() {
+  return 42;
+}
+
+function goodbye() {
+  return 'Goodbye Cruel World!';
+}
+
+exports.hello = hello;
+exports.goodbye = goodbye;
---
+++ index.js
@@ -0,0 +1,8 @@
+const { hello, goodbye } = require('./actions');
+
+const main = async () => {
+  await hello();
+  goodbye();
+};
+
+main().then(console.log('Hello World!'));
